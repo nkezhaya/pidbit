e77f21dc27d582a3575579044685cfc67827f51c
Nick Kezhaya <nick@whitepaperclip.com>
2025-06-12 10:13:34 -0500

    Filter k8s reconnect logs

--- config/config.exs
+++ config/config.exs
@@ -63,7 +63,10 @@ config :tailwind,
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id],
+  compile_time_purge_matching: [
+    [library: :k8s]
+  ]
 
 config :phoenix, :json_library, JSON
 
